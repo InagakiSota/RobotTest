f0bf11559c7a80cf8cc81d7084d8ef78b1543518
InagakiSota <inazoo.jr@gmail.com>
2021-05-14 16:33:22 +0900

    資料ひとまず完成

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -6,50 +6,64 @@ using UnityEngine.UI;
 public class RobotController : MonoBehaviour
 {
 	Rigidbody rb;
-	//ジャンプ力
-	[SerializeField] float JUMP_FORCE;
-	bool m_isJump;
 
-	public CameraController cameraScript;
-
-	//カメラ振動の時間
-	[SerializeField] float m_shakeDuration = 0.25f;
-	//カメラ振動の強さ
-	[SerializeField] float m_shakeMagnitude = 0.1f;
-	//旋回速度(一秒間に回転する角度)
-	[SerializeField] float m_trunSpeed = 1.0f;
+	//カメラのスクリプト
+	[SerializeField]CameraController cameraScript;
 	//着地エフェクト
 	[SerializeReference] ParticleSystem m_shockWave;
-	//歩行スピード
-	[SerializeField] float m_walkSpeed = 10.0f;
+	//ジャンプの溜めゲージのスライダー
+	[SerializeField]Slider m_jumpSlider;
+	//ブースト容量のスライダー
+	[SerializeField]Slider m_boostCapSlider;
+	//ブースト容量のゲージの画像
+	[SerializeField]Image m_boostGageImage;
+
+	//ジャンプ力
+	[SerializeField] float JUMP_FORCE;
 	//着地硬直のタイマー
 	float m_landingRigidityTimer = 0.0f;
 	//着地硬直の時間
 	[SerializeField] float LANDING_RIGIDITY_TIME = 0.25f;
-
+	//ジャンプの溜めゲージの最大値
+	[SerializeField] float JUMP_GAGE_MAX = 2.0f;
+	//ジャンプの溜めの上昇値
+	[SerializeField] float JUMP_CHARGE_UP_VALUE = 0.5f;
+	//ジャンプのゲージ消費量
+	[SerializeField] float JUMP_GAGE_CONSUMPTION = 10.0f;
+	//カメラ振動の時間
+	[SerializeField] float SHAKE_DURATION = 0.25f;
+	//カメラ振動の強さ
+	[SerializeField] float SHAKE_MAGNITUDE = 0.1f;
+	//旋回速度(一秒間に回転する角度)
+	[SerializeField] float TRUN_SPEED = 1.0f;
+	//歩行スピード
+	[SerializeField] float WALK_SPEED = 10.0f;
 	//最大ブースト速度
 	[SerializeField] float BOOST_SPEED_MAX;
-
-	//ジャンプの溜めゲージのスライダー
-	[SerializeField] Slider m_boostSlider;
-
+	//ブースト移動の速度の上昇値
+	[SerializeField] float BOOST_SPEED_UP_VALUE;
+	//ブースト容量のゲージの最大値
+	[SerializeField] float BOOST_GAGE_MAX = 100.0f;
+	//ブースト容量ゲージの減少量
+	[SerializeField] float BOOST_GAGE_DOWN_VALUE = 1.0f;
+	//ブースト容量ゲージの上昇量
+	[SerializeField] float BOOST_GAGE_UP_VALUE = 3.0f;
+	//初期座標(リスポーン座標)
+	[SerializeField] Vector3 START_POS = new Vector3(0, 20.0f, 0);
 
 	//X方向の移動量
 	float m_velX;
 	//Z方向の移動量
 	float m_velZ;
 
+	//ジャンプしているかどうかのフラグ
+	bool m_isJump;
 	//着地フラグ
 	bool m_isStanding;
-
-	//移動量
-	Vector3 vel;
-
 	//着地硬直のタイマーのフラグ
 	bool m_isRigidity = false;
-
-	//初期座標(リスポーン座標)
-	[SerializeField] Vector3 START_POS = new Vector3(0, 20.0f, 0);
+	//オーバーヒートのフラグ
+	bool m_isOverHeat = false;
 
 
 	// Start is called before the first frame update
@@ -62,9 +76,10 @@ public class RobotController : MonoBehaviour
 		m_isJump = false;
 		m_landingRigidityTimer = LANDING_RIGIDITY_TIME;
 		m_isRigidity = false;
-		m_boostSlider.maxValue = 2.0f;
-		m_boostSlider.gameObject.SetActive(false);
+		m_jumpSlider.maxValue = JUMP_GAGE_MAX;
+		m_jumpSlider.gameObject.SetActive(false);
 		this.transform.position = START_POS;
+		m_boostCapSlider.value = m_boostCapSlider.maxValue = BOOST_GAGE_MAX;
 
 	}
 
@@ -75,8 +90,11 @@ public class RobotController : MonoBehaviour
 
 		Transform trans = this.transform;
 
-		//ジャンプ関数
-		Jump();
+		//オーバーヒート状態でなければジャンプ
+		if (m_isOverHeat == false)
+		{
+			Jump();
+		}
 
 		//着地硬直
 		if (m_isRigidity == true)
@@ -94,7 +112,6 @@ public class RobotController : MonoBehaviour
 				m_isRigidity = false;
 				m_landingRigidityTimer = LANDING_RIGIDITY_TIME;
 				///////////////////////////////////
-
 			}
 		}
 
@@ -102,36 +119,34 @@ public class RobotController : MonoBehaviour
 		if (m_isStanding == true && m_isRigidity == false)
 		{
 			//歩行
-			if (Input.GetKey(KeyCode.LeftShift) != true)
+			if (Input.GetKey(KeyCode.LeftShift) != true || m_isOverHeat == true)
 			{
 				Walk();
 
-				Debug.Log("Walk");
 			}
 
-			//LShift入力でブースト移動
-			else if (Input.GetKey(KeyCode.LeftShift) == true)
+			//LShift入力かつオーバーヒート状態でなければブースト移動
+			else if (Input.GetKey(KeyCode.LeftShift) == true && m_isOverHeat == false)
 			{
 				Boost();
-				Debug.Log("Boost");
 			}
 
 			//右旋回
-			if (Input.GetKey(KeyCode.E))
+			if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.RightArrow))
 			{
 				///////////////////////////////////
 				//ココ↓に記述
-				trans.Rotate(new Vector3(0.0f, m_trunSpeed * Time.deltaTime, 0.0f));
+				trans.Rotate(new Vector3(0.0f, TRUN_SPEED * Time.deltaTime, 0.0f));
 				///////////////////////////////////
 
 
 			}
 			//左旋回
-			if (Input.GetKey(KeyCode.Q))
+			if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow))
 			{
 				///////////////////////////////////
 				//ココ↓に記述
-				trans.Rotate(new Vector3(0.0f, -m_trunSpeed * Time.deltaTime, 0.0f));
+				trans.Rotate(new Vector3(0.0f, -TRUN_SPEED * Time.deltaTime, 0.0f));
 				///////////////////////////////////
 			}
 
@@ -149,6 +164,10 @@ public class RobotController : MonoBehaviour
 			this.transform.position = START_POS;
 			rb.velocity = Vector3.zero;
 		}
+
+		//ブーストゲージの処理
+		BoostGage();
+
 	}
 
 	//////////////////////////////////////
@@ -164,7 +183,7 @@ public class RobotController : MonoBehaviour
 		{
 			//////////////////////////////
 			//ココ↓に記述
-			m_velX = m_walkSpeed;
+			m_velX = WALK_SPEED;
 			//////////////////////////////
 		}
 		//左方向(-)に移動量を代入する
@@ -172,7 +191,7 @@ public class RobotController : MonoBehaviour
 		{
 			//////////////////////////////
 			//ココ↓に記述
-			m_velX = -m_walkSpeed;
+			m_velX = -WALK_SPEED;
 			//////////////////////////////
 		}
 		else m_velX = 0.0f;
@@ -183,7 +202,7 @@ public class RobotController : MonoBehaviour
 		{
 			//////////////////////////////
 			//ココ↓に記述
-			m_velZ = m_walkSpeed;
+			m_velZ = WALK_SPEED;
 			//////////////////////////////
 
 		}
@@ -192,7 +211,7 @@ public class RobotController : MonoBehaviour
 		{
 			//////////////////////////////
 			//ココ↓に記述
-			m_velZ = -m_walkSpeed;
+			m_velZ = -WALK_SPEED;
 			//////////////////////////////
 		}
 		else m_velZ = 0.0f;
@@ -209,17 +228,21 @@ public class RobotController : MonoBehaviour
 		//右移動　X方向の移動速度を加算する
 		if (Input.GetKey(KeyCode.D))
 		{
-			m_velX += 10.0f * Time.deltaTime;
+			m_velX += BOOST_SPEED_UP_VALUE * Time.deltaTime;
 			//最大値を越えたら最大値を代入する
 			if (m_velX >= BOOST_SPEED_MAX) m_velX = BOOST_SPEED_MAX;
+
+
 		}
 
 		//左移動　X方向の移動速度を減算する
 		else if (Input.GetKey(KeyCode.A))
+
 		{
-			m_velX -= 10.0f * Time.deltaTime;
+			m_velX -= BOOST_SPEED_UP_VALUE * Time.deltaTime;
 			//最大値を越えたら最大値を代入する
 			if (m_velX <= -BOOST_SPEED_MAX) m_velX = -BOOST_SPEED_MAX;
+
 		}
 		//左右どちらかの入力がなければ減速
 		else
@@ -240,16 +263,18 @@ public class RobotController : MonoBehaviour
 		//前移動　Z方向の移動速度を加算する
 		if (Input.GetKey(KeyCode.W))
 		{
-			m_velZ += 10.0f * Time.deltaTime;
+			m_velZ += BOOST_SPEED_UP_VALUE * Time.deltaTime;
 			//最大値を越えたら最大値を代入する
 			if (m_velZ >= BOOST_SPEED_MAX) m_velZ = BOOST_SPEED_MAX;
+
 		}
 		//後移動　Z方向の移動速度を減算する
 		else if (Input.GetKey(KeyCode.S))
 		{
-			m_velZ -= 10.0f * Time.deltaTime;
+			m_velZ -= BOOST_SPEED_UP_VALUE * Time.deltaTime;
 			//最大値を越えたら最大値を代入する
 			if (m_velZ <= -BOOST_SPEED_MAX) m_velZ = -BOOST_SPEED_MAX;
+
 		}
 		//前後どちらかの入力がなければ減速
 		else
@@ -264,7 +289,6 @@ public class RobotController : MonoBehaviour
 				m_velZ -= 1.0f;
 				if (m_velZ <= 0.0f) m_velZ = 0.0f;
 			}
-
 		}
 
 	}
@@ -279,14 +303,14 @@ public class RobotController : MonoBehaviour
 		//スペースキー入力でジャンプのゲージ溜め
 		if (Input.GetKey(KeyCode.Space) && m_isJump == false && m_isStanding == true && m_isRigidity == false)
 		{
-			m_boostSlider.value += 0.5f * Time.deltaTime;
+			m_jumpSlider.value += JUMP_CHARGE_UP_VALUE * Time.deltaTime;
 			//溜めゲージを表示する
-			m_boostSlider.gameObject.SetActive(true);
+			m_jumpSlider.gameObject.SetActive(true);
 
 		}
 
 		//ジャンプ力を計算する
-		float jumpForce = JUMP_FORCE * (1.0f + m_boostSlider.value);
+		float jumpForce = JUMP_FORCE * (1.0f + m_jumpSlider.value);
 
 		//スペースキーリリースでジャンプ
 		if (Input.GetKeyUp(KeyCode.Space) && m_isJump == false && m_isStanding == true && m_isRigidity == false)
@@ -296,12 +320,14 @@ public class RobotController : MonoBehaviour
 			rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
 			//////////////////////////////
 
+			//ブースト容量を消費
+			m_boostCapSlider.value -= JUMP_GAGE_CONSUMPTION;
 		}
 	}
 
 		private void FixedUpdate()
 	{
-		//rb.AddForce(vel, ForceMode.Force);
+
 	}
 
 	private void OnTriggerEnter(Collider other)
@@ -313,9 +339,8 @@ public class RobotController : MonoBehaviour
 		///////////////////////////////
 		//カメラを揺らす
 		//ココ↓に記述
-		cameraScript.Shake(m_shakeDuration, m_shakeMagnitude);
+		cameraScript.Shake(SHAKE_DURATION, SHAKE_MAGNITUDE);
 		///////////////////////////////
-
 
 		//着地エフェクトの再生
 		//m_shockWave.Play();
@@ -324,10 +349,10 @@ public class RobotController : MonoBehaviour
 		m_isRigidity = true;
 
 		//溜めゲージを非表示にする
-		m_boostSlider.gameObject.SetActive(false);
+		m_jumpSlider.gameObject.SetActive(false);
 
 		//溜めゲージを0にする
-		m_boostSlider.value = 0.0f;
+		m_jumpSlider.value = 0.0f;
 
 	}
 
@@ -339,5 +364,53 @@ public class RobotController : MonoBehaviour
 	private void OnTriggerExit(Collider other)
 	{
 		m_isStanding = false;
+	}
+
+
+	///////////////////////////////
+	//ブーストゲージの処理
+	//引数：なし
+	//戻り値：なし
+	//////////////////////////////
+	void BoostGage()
+	{
+		//ブースト移動中はゲージを減らす
+		if (Input.GetKey(KeyCode.LeftShift) && m_isStanding == true && m_isOverHeat == false)
+		{
+			if (Input.GetKey(KeyCode.W) ||
+				Input.GetKey(KeyCode.A) ||
+				Input.GetKey(KeyCode.S) ||
+				Input.GetKey(KeyCode.D))
+			{
+				m_boostCapSlider.value -= BOOST_GAGE_DOWN_VALUE * Time.deltaTime;
+			}
+			//ブースト移動解除でゲージ回復
+			else m_boostCapSlider.value += BOOST_GAGE_UP_VALUE * Time.deltaTime;
+		}
+		//ブースト移動解除でゲージ回復
+		else if (Input.GetKey(KeyCode.LeftShift) == false && m_isStanding == true)
+		{
+			m_boostCapSlider.value += BOOST_GAGE_UP_VALUE * Time.deltaTime;
+
+		}
+
+		//ゲージが0になったらオーバーヒートさせる
+		if(m_boostCapSlider.value <= 0.0f)
+		{
+			m_isOverHeat = true;
+		}
+
+		//オーバーヒート状態になったらブースト容量ゲージを赤色にする
+		if (m_isOverHeat == true)
+		{
+			m_boostGageImage.color = Color.red;
+			m_boostCapSlider.value += (BOOST_GAGE_UP_VALUE * 2.0f) * Time.deltaTime;
+			if (m_boostCapSlider.value >= BOOST_GAGE_MAX) m_isOverHeat = false;
+		}
+		else
+		{
+			m_boostGageImage.color = Color.yellow;
+		}
+
 	}
 }
--- Assets/Scripts/RobotController_Unfinished.cs
+++ Assets/Scripts/RobotController_Unfinished.cs
@@ -5,51 +5,65 @@ using UnityEngine.UI;
 
 public class RobotController_Unfinished : MonoBehaviour
 {
-
 	Rigidbody rb;
-	//ジャンプ力
-	[SerializeField] float JUMP_FORCE;
-	bool m_isJump;
 
-	public CameraController cameraScript;
-
-	//カメラ振動の時間
-	[SerializeField] float m_shakeDuration = 0.25f;
-	//カメラ振動の強さ
-	[SerializeField] float m_shakeMagnitude = 0.1f;
-	//旋回速度(一秒間に回転する角度)
-	[SerializeField] float m_trunSpeed = 1.0f;
+	//カメラのスクリプト
+	[SerializeField] CameraController cameraScript;
 	//着地エフェクト
 	[SerializeReference] ParticleSystem m_shockWave;
-	//歩行スピード
-	[SerializeField] float m_walkSpeed = 10.0f;
+	//ジャンプの溜めゲージのスライダー
+	[SerializeField] Slider m_jumpSlider;
+	//ブースト容量のスライダー
+	[SerializeField] Slider m_boostCapSlider;
+	//ブースト容量のゲージの画像
+	[SerializeField] Image m_boostGageImage;
+
+	//ジャンプ力
+	[SerializeField] float JUMP_FORCE;
 	//着地硬直のタイマー
 	float m_landingRigidityTimer = 0.0f;
 	//着地硬直の時間
 	[SerializeField] float LANDING_RIGIDITY_TIME = 0.25f;
-
+	//ジャンプの溜めゲージの最大値
+	[SerializeField] float JUMP_GAGE_MAX = 2.0f;
+	//ジャンプの溜めの上昇値
+	[SerializeField] float JUMP_CHARGE_UP_VALUE = 0.5f;
+	//ジャンプのゲージ消費量
+	[SerializeField] float JUMP_GAGE_CONSUMPTION = 10.0f;
+	//カメラ振動の時間
+	[SerializeField] float SHAKE_DURATION = 0.25f;
+	//カメラ振動の強さ
+	[SerializeField] float SHAKE_MAGNITUDE = 0.1f;
+	//旋回速度(一秒間に回転する角度)
+	[SerializeField] float TRUN_SPEED = 1.0f;
+	//歩行スピード
+	[SerializeField] float WALK_SPEED = 10.0f;
 	//最大ブースト速度
 	[SerializeField] float BOOST_SPEED_MAX;
-
-	//ジャンプの溜めゲージのスライダー
-	[SerializeField] Slider m_boostSlider;
+	//ブースト移動の速度の上昇値
+	[SerializeField] float BOOST_SPEED_UP_VALUE;
+	//ブースト容量のゲージの最大値
+	[SerializeField] float BOOST_GAGE_MAX = 100.0f;
+	//ブースト容量ゲージの減少量
+	[SerializeField] float BOOST_GAGE_DOWN_VALUE = 1.0f;
+	//ブースト容量ゲージの上昇量
+	[SerializeField] float BOOST_GAGE_UP_VALUE = 3.0f;
+	//初期座標(リスポーン座標)
+	[SerializeField] Vector3 START_POS = new Vector3(0, 20.0f, 0);
 
 	//X方向の移動量
 	float m_velX;
 	//Z方向の移動量
 	float m_velZ;
-
+	//ジャンプしているかどうかのフラグ
+	bool m_isJump;
 	//着地フラグ
 	bool m_isStanding;
-
-	//移動量
-	Vector3 vel;
-
 	//着地硬直のタイマーのフラグ
 	bool m_isRigidity = false;
+	//オーバーヒートのフラグ
+	bool m_isOverHeat = false;
 
-	//初期座標(リスポーン座標)
-	[SerializeField] Vector3 START_POS = new Vector3(0,20.0f,0);
 
 
 	// Start is called before the first frame update
@@ -62,9 +76,10 @@ public class RobotController_Unfinished : MonoBehaviour
 		m_isJump = false;
 		m_landingRigidityTimer = LANDING_RIGIDITY_TIME;
 		m_isRigidity = false;
-		m_boostSlider.maxValue = 2.0f;
-		m_boostSlider.gameObject.SetActive(false);
+		m_jumpSlider.maxValue = JUMP_GAGE_MAX;
+		m_jumpSlider.gameObject.SetActive(false);
 		this.transform.position = START_POS;
+		m_boostCapSlider.value = m_boostCapSlider.maxValue = BOOST_GAGE_MAX;
 	}
 
 	// Update is called once per frame
@@ -73,8 +88,11 @@ public class RobotController_Unfinished : MonoBehaviour
 
 		Transform trans = this.transform;
 
-		//ジャンプ関数
-		Jump();
+		//オーバーヒート状態でなければジャンプ
+		if (m_isOverHeat == false)
+		{
+			Jump();
+		}
 
 		//着地硬直
 		if (m_isRigidity == true)
@@ -98,19 +116,20 @@ public class RobotController_Unfinished : MonoBehaviour
 		if (m_isStanding == true && m_isRigidity == false)
 		{
 			//歩行
-			if (Input.GetKey(KeyCode.LeftShift) != true)
+			if (Input.GetKey(KeyCode.LeftShift) != true || m_isOverHeat == true)
 			{
 				Walk();
+
 			}
 
-			//LShift入力でブースト移動
-			else if (Input.GetKey(KeyCode.LeftShift) == true)
+			//LShift入力かつオーバーヒート状態でなければブースト移動
+			else if (Input.GetKey(KeyCode.LeftShift) == true && m_isOverHeat == false)
 			{
 				Boost();
 			}
 
 			//右旋回
-			if (Input.GetKey(KeyCode.E))
+			if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.RightArrow))
 			{
 				//////////////////////////////
 				//ココ↓に記述
@@ -119,7 +138,7 @@ public class RobotController_Unfinished : MonoBehaviour
 
 			}
 			//左旋回
-			if (Input.GetKey(KeyCode.Q))
+			if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow))
 			{
 				//////////////////////////////
 				//ココ↓に記述
@@ -141,6 +160,9 @@ public class RobotController_Unfinished : MonoBehaviour
 			this.transform.position = START_POS;
 			rb.velocity = Vector3.zero;
 		}
+
+		//ブーストゲージの処理
+		BoostGage();
 
 	}
 
@@ -201,20 +223,21 @@ public class RobotController_Unfinished : MonoBehaviour
 		//右移動　X方向の移動速度を加算する
 		if (Input.GetKey(KeyCode.D))
 		{
-			m_velX += 10.0f * Time.deltaTime;
+			//////////////////////////////
+			//ココ↓に記述
+
+			//////////////////////////////
 			//最大値を越えたら最大値を代入する
 			if (m_velX >= BOOST_SPEED_MAX) m_velX = BOOST_SPEED_MAX;
 		}
-		//else
-		//{
-		//	m_velX -= 1.0f * Time.deltaTime;
-		//	if (m_velX <= 0.0f) m_velX = 0.0f;
-		//}
 
 		//左移動　X方向の移動速度を減算する
 		else if (Input.GetKey(KeyCode.A))
 		{
-			m_velX -= 10.0f * Time.deltaTime;
+			//////////////////////////////
+			//ココ↓に記述
+
+			//////////////////////////////
 			//最大値を越えたら最大値を代入する
 			if (m_velX <= -BOOST_SPEED_MAX) m_velX = -BOOST_SPEED_MAX;
 		}
@@ -237,14 +260,20 @@ public class RobotController_Unfinished : MonoBehaviour
 		//前移動　Z方向の移動速度を加算する
 		if (Input.GetKey(KeyCode.W))
 		{
-			m_velZ += 10.0f * Time.deltaTime;
+			//////////////////////////////
+			//ココ↓に記述
+
+			//////////////////////////////
 			//最大値を越えたら最大値を代入する
 			if (m_velZ >= BOOST_SPEED_MAX) m_velZ = BOOST_SPEED_MAX;
 		}
 		//後移動　Z方向の移動速度を減算する
 		else if (Input.GetKey(KeyCode.S))
 		{
-			m_velZ -= 10.0f * Time.deltaTime;
+			//////////////////////////////
+			//ココ↓に記述
+
+			//////////////////////////////
 			//最大値を越えたら最大値を代入する
 			if (m_velZ <= -BOOST_SPEED_MAX) m_velZ = -BOOST_SPEED_MAX;
 		}
@@ -276,14 +305,14 @@ public class RobotController_Unfinished : MonoBehaviour
 		//スペースキー入力でジャンプのゲージ溜め
 		if(Input.GetKey(KeyCode.Space) && m_isJump == false && m_isStanding == true && m_isRigidity == false)
 		{
-			m_boostSlider.value += 0.5f * Time.deltaTime;
+			m_jumpSlider.value += JUMP_CHARGE_UP_VALUE * Time.deltaTime;
 			//溜めゲージを表示する
-			m_boostSlider.gameObject.SetActive(true);
+			m_jumpSlider.gameObject.SetActive(true);
 
 		}
 
 		//ジャンプ力を計算する
-		float jumpForce = JUMP_FORCE * (1.0f + m_boostSlider.value);
+		float jumpForce = JUMP_FORCE * (1.0f + m_jumpSlider.value);
 
 		//スペースキーリリースでジャンプ
 		if (Input.GetKeyUp(KeyCode.Space) && m_isJump == false && m_isStanding == true && m_isRigidity == false)
@@ -315,15 +344,20 @@ public class RobotController_Unfinished : MonoBehaviour
 
 		///////////////////////////////
 
+		///////////////////////////////
+		//着地エフェクトの再生
+		//ココ↓に記述
+
+		///////////////////////////////
 
 		//着地硬直のフラグを立てる
 		m_isRigidity = true;
 
 		//溜めゲージを非表示にする
-		m_boostSlider.gameObject.SetActive(false);
+		m_jumpSlider.gameObject.SetActive(false);
 
 		//溜めゲージを0にする
-		m_boostSlider.value = 0.0f;
+		m_jumpSlider.value = 0.0f;
 
 	}
 
@@ -336,4 +370,47 @@ public class RobotController_Unfinished : MonoBehaviour
 	{
 		m_isStanding = false;
 	}
+
+	void BoostGage()
+	{
+		//ブースト移動中はゲージを減らす
+		if (Input.GetKey(KeyCode.LeftShift) && m_isStanding == true && m_isOverHeat == false)
+		{
+			if (Input.GetKey(KeyCode.W) ||
+				Input.GetKey(KeyCode.A) ||
+				Input.GetKey(KeyCode.S) ||
+				Input.GetKey(KeyCode.D))
+			{
+				m_boostCapSlider.value -= BOOST_GAGE_DOWN_VALUE * Time.deltaTime;
+			}
+			//ブースト移動解除でゲージ回復
+			else m_boostCapSlider.value += BOOST_GAGE_UP_VALUE * Time.deltaTime;
+		}
+		//ブースト移動解除でゲージ回復
+		else if (Input.GetKey(KeyCode.LeftShift) == false && m_isStanding == true)
+		{
+			m_boostCapSlider.value += BOOST_GAGE_UP_VALUE * Time.deltaTime;
+
+		}
+
+		//ゲージが0になったらオーバーヒートさせる
+		if (m_boostCapSlider.value <= 0.0f)
+		{
+			m_isOverHeat = true;
+		}
+
+		//オーバーヒート状態になったらブースト容量ゲージを赤色にする
+		if (m_isOverHeat == true)
+		{
+			m_boostGageImage.color = Color.red;
+			m_boostCapSlider.value += (BOOST_GAGE_UP_VALUE * 2.0f) * Time.deltaTime;
+			if (m_boostCapSlider.value >= BOOST_GAGE_MAX) m_isOverHeat = false;
+		}
+		else
+		{
+			m_boostGageImage.color = Color.yellow;
+		}
+
+	}
+
 }
